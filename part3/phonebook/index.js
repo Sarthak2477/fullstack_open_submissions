779729af5f86d6682ff6d2fdda1d145154b5ed06
sarthak2477 <sarthakp8074@gmail.com>
2026-01-31 19:23:12 +0530

    changes for deployment

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -85,5 +85,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-app.listen(3000)
-console.log("Server listening on port 3000...")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
