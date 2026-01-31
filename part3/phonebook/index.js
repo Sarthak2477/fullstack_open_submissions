adb3b2b8efd83434cc7cd86eb717a3a732f5da4d
sarthak2477 <sarthakp8074@gmail.com>
2026-01-31 19:18:54 +0530

    Part submission

---
+++ part3/phonebook/index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+let persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+const app = express()
+app.use(express.json())
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body);
+});
+
+app.use(cors())
+// Use the token in your format
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    res.send(`
+        <div>
+            <p>Phonebook has info for ${persons.length} people</p>
+            <p>${new Date()}</p>
+        </div>
+    `)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const person = req.body
+
+    if (!person || !person.name || !person.number) {
+        return res.status(400).json({ error: 'name and number are required' })
+    }
+
+    if (persons.find(p => p.name === person.name)) {
+        return res.status(400).json({ error: 'name must be unique' })
+    }
+
+    person.id = Math.floor(Math.random() * 1000).toString()
+    persons.push(person)
+    res.json(person)
+})
+
+app.listen(3000)
+console.log("Server listening on port 3000...")
